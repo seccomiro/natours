13529f205352d0e417e01d2bf885b5bec15cb0f8
Diego Stiehl <diego.stiehl@ifpr.edu.br>
2019-08-06 23:07:52 -0300

    Iniciando com Mongo

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,14 +1,4 @@
-const fs = require('fs');
 const Tour = require('./../models/tourModel');
-
-const toursFile = `${__dirname}/../dev-data/data/tours-simple.json`;
-const tours = JSON.parse(fs.readFileSync(toursFile));
-
-const saveTours = callback => {
-  fs.writeFile(toursFile, JSON.stringify(tours), callback);
-};
-
-const findTour = (id, byIndex = false) => tours[`find${byIndex ? 'Index' : ''}`](tour => tour.id === parseInt(id, 10));
 
 const invalidIdResponse = res =>
   res.status(404).json({
@@ -16,30 +6,13 @@ const invalidIdResponse = res =>
     message: 'Invalid ID'
   });
 
-exports.checkID = (req, res, next, id) => {
-  const tour = findTour(id);
-  if (!tour) return invalidIdResponse(res);
-  req.tour = tour;
-  next();
-};
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price'
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: { tours }
-  });
+  // res.status(200).json({
+  //   status: 'success',
+  //   requestedAt: req.requestTime,
+  //   results: tours.length,
+  //   data: { tours }
+  // });
 };
 
 exports.getTour = (req, res) => {
@@ -51,16 +24,20 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  saveTours(() => {
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
     res.status(201).json({
       status: 'success',
       data: { tour: newTour }
     });
-  });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data set'
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
@@ -71,24 +48,22 @@ exports.updateTour = (req, res) => {
     }
   });
 
-  saveTours(() => {
-    res.status(201).json({
-      status: 'success',
-      data: { tour }
-    });
-  });
+  // saveTours(() => {
+  //   res.status(201).json({
+  //     status: 'success',
+  //     data: { tour }
+  //   });
+  // });
 };
 
 exports.deleteTour = (req, res) => {
-  const { tour } = req;
-  const i = findTour(tour.id, true);
-
-  tours.splice(i, 1);
-
-  saveTours(() => {
-    res.status(204).json({
-      status: 'success',
-      data: null
-    });
-  });
+  // const { tour } = req;
+  // const i = findTour(tour.id, true);
+  // // tours.splice(i, 1);
+  // saveTours(() => {
+  //   res.status(204).json({
+  //     status: 'success',
+  //     data: null
+  //   });
+  // });
 };
