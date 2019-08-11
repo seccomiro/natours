8f3b3a3dc0cf52ae096bc2df56435f942018157c
Diego Stiehl <diego.stiehl@ifpr.edu.br>
2019-08-11 17:15:47 -0300

    Validaçoes

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,17 +1,50 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
-    name: { type: String, required: [true, 'A tour must have a name'], unique: true, trim: true },
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+      maxlength: [40, 'A tour name must have a maximum of 40 characters'],
+      minlength: [10, 'A tour name must have a minimum of 10 characters']
+      // validate: {
+      //   message: 'Name must have only letters',
+      //   validator: validator.isAlpha
+      // }
+    },
     slug: String,
     duration: { type: Number, required: [true, 'A tour must have a duration'] },
     maxGroupSize: { type: Number, required: [true, 'A tour must have a group size'] },
-    difficulty: { type: String, required: [true, 'A tour must have a difficulty'] },
-    ratingsAverage: { type: Number, default: 4.5 },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty must be: easy, medium or difficult'
+      }
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be 1 or above'],
+      max: [5, 'Rating must be 5 or less']
+    },
     ratingsQuantity: { type: Number, default: 0 },
     price: { type: Number, required: [true, 'A tour must have a price'] },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function(val) {
+          // "this" only points to current doc on NEW document creation
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) must be below regular price'
+      }
+    },
     summary: { type: String, trim: true },
     description: { type: String, trim: true, required: [true, 'A tour must have a description'] },
     imageCover: { type: String, required: [true, 'A tour must have a cover image'] },
