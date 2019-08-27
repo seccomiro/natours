3d874596792655d0c21a83642ddbb36c45e6e605
Diego Stiehl <seccomiro@gmail.com>
2019-08-26 21:24:22 -0300

    Made generic error handling

--- server.js
+++ server.js
@@ -17,7 +17,25 @@ mongoose
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   const env = process.env.NODE_ENV || 'production';
   console.log(`[${env.toUpperCase()}] App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.error(err.name, '|', err.message);
+  console.error('UNHANDLED REJECTION! SHUTTING DOWN...');
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// Better if settled on the top of the code
+// Won't be called if the exception occurs inside an HTTP request
+process.on('uncaughtException', err => {
+  console.error(err.name, '|', err.message);
+  console.error('UNCAUGHT EXCEPTION! SHUTTING DOWN...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
